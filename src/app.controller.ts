324639caeb7013d74e3eeb341c39ac20bd6c0ea6
dinh anh tuan <tuan.dinh@desports.gg>
2022-11-04 14:01:53 +0700

    connect mqtt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { MqttService } from 'nest-mqtt';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(MqttService) private readonly mqttService: MqttService,
+  ) {
+    // client.connect();
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  async testPublish() {
+    const res = await this.mqttService.publish('testtopic/1', {
+      foo: 'tuan',
+    });
+
+    return {
+      data : res
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,27 @@ import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './auth/local.strategy';
 import { RollCallModule } from './roll-call/roll-call.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MqttModule } from 'nest-mqtt';
+import { IotModule } from './iot/iot.module';
+
 @Module({
   imports: [
+    MqttModule.forRoot({
+      host: 'dfbd389814bd4a7a9897038c99700f75.s2.eu.hivemq.cloud',
+      port: 8883,
+      username: 'redutdep13',
+      password: 'redutdep17',
+      protocol: 'mqtts',
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
     UserModule,
     RollCallModule,
     AuthModule,
+    IotModule,
   ],
   controllers: [AppController],
-  providers: [AppService  ],
+  providers: [AppService],
 })
 export class AppModule {}
