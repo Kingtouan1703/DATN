5a811d48900bd53b72b2e12dcf4dd433a3bfeabf
dinh anh tuan <tuan.dinh@desports.gg>
2022-11-06 21:54:36 +0700

    init socket to deploy

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Inject, Logger } from '@nestjs/common';
 import {
   MessageBody,
   OnGatewayInit,
@@ -6,10 +6,13 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { MqttService, Subscribe, Payload } from 'nest-mqtt';
 import { Server } from 'socket.io';
+import { RoomSensorPayload, RoomTopic } from 'src/room/topic/room.topic';
 
 @WebSocketGateway({ cors: '*:*' })
 export class SocketGateway implements OnGatewayInit {
+  @Inject(MqttService) private readonly mqttService: MqttService;
   @WebSocketServer()
   server: Server;
   private logger: Logger = new Logger('WSGateway');
@@ -19,5 +22,10 @@ export class SocketGateway implements OnGatewayInit {
   @SubscribeMessage('test_message')
   listenForMessages(@MessageBody() data: string) {
     this.server.sockets.emit('receive_message', data);
+  }
+  // mqtt => socket emit
+  @Subscribe(RoomTopic.SUB_SENSOR)
+  emitTest(@Payload() payload: RoomSensorPayload) {
+    this.server.sockets.emit('room_sensor', payload);
   }
 }
